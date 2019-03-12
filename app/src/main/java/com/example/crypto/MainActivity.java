5794a2d9bf41fa89a7b7119f8bbbd47ade468a2b
mikal-pc <ventusboy13@gmail.com>
2019-03-12 02:58:21 -0400

    testing

--- app/src/main/java/com/example/crypto/MainActivity.java
+++ app/src/main/java/com/example/crypto/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.crypto;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,10 +18,18 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    //vars
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls =new ArrayList<>();
 
     RequestQueue rq;
 
@@ -36,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         rq = Volley.newRequestQueue(this);
         buttonget=findViewById(R.id.button);
+        initImageBitmaps();;
 
 
 
@@ -62,19 +73,34 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void initImageBitmaps(){
+        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
+        mNames.add("Havasu Falls");
+
+        initRecyclerView();
+
+    }
+
+    private void initRecyclerView(){
+
+        RecyclerView recyclerView = findViewById(R.id.rView);
+        recycleviewadapter adapter = new recycleviewadapter(this, mNames, mImageUrls);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
     public void jsonrequest(String coin){
 
         //coin="eth";
 
         final String coin1 = coin;
 
-
-
-        String url = "https://min-api.cryptocompare.com/data/price?fsym="+ coin.toUpperCase() + "&tsyms=USD&api_key=dcb8ee8a020fc0aae10aca956d92c330d4fa95e38433ba76caf58291f4b199a2";
+        String url = "https://min-api.cryptocompare.com/data/generalinfo?fsyms="+ coin.toUpperCase() + "&tsyms=USD&api_key=dcb8ee8a020fc0aae10aca956d92c330d4fa95e38433ba76caf58291f4b199a2";
 
         //TextView text=(TextView)findViewById(R.id.usd_price);
 
-       // text.setText("test1");
+        // text.setText("test1");
 
         //TextView text=(TextView)findViewById(R.id.usd_price);
         //text.setText("Some text....");
@@ -88,10 +114,12 @@ public class MainActivity extends AppCompatActivity {
                         TextView text2=(TextView)findViewById(R.id.usd_price);
 
                         try {
-                            String name = (String) response.getString("USD");
 
-                            //text.setText(response.get("USD"));
-                            text2.setText(name);
+
+                            String price = (String) response.getString("USD");
+
+
+                            text2.setText(price);
                         } catch (JSONException e) {
 
                             text2.setText(coin1);
@@ -111,6 +139,50 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
+
+
+
+        /*String url = "https://min-api.cryptocompare.com/data/price?fsym="+ coin.toUpperCase() + "&tsyms=USD&api_key=dcb8ee8a020fc0aae10aca956d92c330d4fa95e38433ba76caf58291f4b199a2";
+
+        //TextView text=(TextView)findViewById(R.id.usd_price);
+
+       // text.setText("test1");
+
+        //TextView text=(TextView)findViewById(R.id.usd_price);
+        //text.setText("Some text....");
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        TextView text2=(TextView)findViewById(R.id.usd_price);
+
+                        try {
+                            String price = (String) response.getString("USD");
+
+                            //text.setText(response.get("USD"));
+                            text2.setText(price);
+                        } catch (JSONException e) {
+
+                            text2.setText(coin1);
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        TextView text=(TextView)findViewById(R.id.usd_price);
+
+                        text.setText("error");
+
+                    }
+                });*/
         rq.add(jsonObjectRequest);
 
     }
---
+++ app/src/main/java/com/example/crypto/recycleviewadapter.java
@@ -0,0 +1,83 @@
+package com.example.crypto;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class recycleviewadapter extends RecyclerView.Adapter<recycleviewadapter.ViewHolder>{
+
+    private static final String TAG = "recycleviewadapter";
+
+    private ArrayList<String> mImageNames =new ArrayList<>();
+    private ArrayList<String> mImages =new ArrayList<>();
+    private Context mContext;
+
+    public recycleviewadapter( Context mContext, ArrayList<String> mImageNames, ArrayList<String> mImages) {
+        this.mImageNames = mImageNames;
+        this.mImages = mImages;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.layoutlistitem, parent, false);
+        ViewHolder holder =new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+        Glide.with(mContext).asBitmap().load(mImages.get(position)).into(holder.image);
+
+        holder.coinName.setText(mImageNames.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on"+ mImageNames.get(position));
+
+                Toast.makeText(mContext, mImageNames.get(position),Toast.LENGTH_SHORT).show();
+                
+                
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return mImageNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView image;
+        TextView coinName;
+        RelativeLayout parentLayout;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image= itemView.findViewById(R.id.image);
+            coinName=itemView.findViewById(R.id.coinCard);
+            parentLayout= itemView.findViewById(R.id.parent_layout);
+
+        }
+    }
+
+}
